38e7305b06da93e848726e6bfdbbc37e46865cdd
James Black <jmblack@waimea.school.nz>
2025-03-26 12:24:37 +1300

    building the map
    mapping deez nuts

--- src/Main.kt
+++ src/Main.kt
@@ -161,7 +161,18 @@ class MainWindow(val app: App, val popUp: SubWindow) : JFrame(), ActionListener 
 class SubWindow(val app: App) : JFrame() {
 
     // Fields to hold the UI elements
-    private lateinit var clicksLabel: JLabel
+//    private lateinit var titleLabel: JLabel
+
+    private lateinit var startMap: JLabel
+    private lateinit var securityMap: JLabel
+    private lateinit var weaponsMap: JLabel
+    private lateinit var labMap: JLabel
+    //========================================//
+    private lateinit var comsMap: JLabel
+    private lateinit var trashMap: JLabel
+    private lateinit var oxygenMap: JLabel
+    private lateinit var gardenMap: JLabel
+    //========================================//
 
     /**
      * Configure the UI and display it
@@ -184,7 +195,7 @@ class SubWindow(val app: App) : JFrame() {
      */
     private fun configureWindow() {
         title = "Map Of Ship"
-        contentPane.preferredSize = Dimension(300, 200)
+        contentPane.preferredSize = Dimension(750, 450)
         isResizable = false
         layout = null
 
@@ -195,13 +206,78 @@ class SubWindow(val app: App) : JFrame() {
      * Populate the UI with UI controls
      */
     private fun addControls() {
-        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 36)
+        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 10)
 
-        clicksLabel = JLabel("Map")
-        clicksLabel.horizontalAlignment = SwingConstants.CENTER
-        clicksLabel.bounds = Rectangle(50, 50, 200, 100)
-        clicksLabel.font = baseFont
-        add(clicksLabel)
+//        titleLabel = JLabel("Map")
+//        titleLabel.horizontalAlignment = SwingConstants.CENTER
+//        titleLabel.bounds = Rectangle(50, 50, 40, 20)
+//        titleLabel.font = baseFont
+//        add(titleLabel)
+
+        securityMap = JLabel("Security")
+        securityMap.horizontalAlignment = SwingConstants.CENTER
+        securityMap.bounds = Rectangle(35, 50, 100, 50)
+        securityMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        securityMap.font = baseFont
+        add(securityMap)
+
+        startMap = JLabel("Start")
+        startMap.horizontalAlignment = SwingConstants.CENTER
+        startMap.bounds = Rectangle(35, 150, 100, 50)
+        startMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        startMap.font = baseFont
+        add(startMap)
+
+        weaponsMap = JLabel("Weapons")
+        weaponsMap.horizontalAlignment = SwingConstants.CENTER
+        weaponsMap.bounds = Rectangle(35, 250, 100, 50)
+        weaponsMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        weaponsMap.font = baseFont
+        add(weaponsMap)
+
+        labMap = JLabel("Lab")
+        labMap.horizontalAlignment = SwingConstants.CENTER
+        labMap.bounds = Rectangle(35, 350, 100, 50)
+        labMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        labMap.font = baseFont
+        add(labMap)
+
+        //=====================================================================//
+
+        comsMap = JLabel("Communications")
+        comsMap.horizontalAlignment = SwingConstants.CENTER
+        comsMap.bounds = Rectangle(170, 50, 100, 50)
+        comsMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        comsMap.font = baseFont
+        add(comsMap)
+
+        trashMap = JLabel("Trash Unit")
+        trashMap.horizontalAlignment = SwingConstants.CENTER
+        trashMap.bounds = Rectangle(170, 150, 100, 50)
+        trashMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        trashMap.font = baseFont
+        add(trashMap)
+
+        oxygenMap = JLabel("Oxygen")
+        oxygenMap.horizontalAlignment = SwingConstants.CENTER
+        oxygenMap.bounds = Rectangle(170, 250, 100, 50)
+        oxygenMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        oxygenMap.font = baseFont
+        add(oxygenMap)
+
+        gardenMap = JLabel("Gardens")
+        gardenMap.horizontalAlignment = SwingConstants.CENTER
+        gardenMap.bounds = Rectangle(170, 350, 100, 50)
+        gardenMap.border = BorderFactory.createLineBorder(Color.GRAY, 3)
+        gardenMap.font = baseFont
+        add(gardenMap)
+
+
+
+
+
+
+
 
     }
 
